7369b4dece60ed29933deb749c03aa6ffa56886f
Wellington5000 <wellingtonsantos5000@gmail.com>
2021-06-28 20:51:21 -0300

    Autenticação adicionada

--- controllers/project.js
+++ controllers/project.js
@@ -1,16 +1,41 @@
 const express = require('express')
 const router = express.Router()
+const Funcionario = require('../models/employee')
+const bcrypt = require('bcrypt')
 
 router.get('/', async (req, res) => {
-    res.render('includes/index', {success: false})
+    res.render('includes/index', { success: false })
 })
 
 router.get('/novo', (req, res) => {
-    res.render('includes/FormNotaFiscal', {success: req.params.valid})
+    res.render('includes/FormNotaFiscal', { success: req.params.valid })
 })
 
 router.get('/consultar', (req, res) => {
-    res.render('includes/Consult', {texto: 'Wellington'})
+    res.render('includes/Consult', { texto: 'Wellington' })
 })
 
+
+router.get('/login', (req, res) => {
+    res.render('includes/formLogin')
+})
+
+router.post('/autenticacao', async (req, res) => {
+    const email = req.body.email
+    const senha = req.body.senha
+
+    var result = await Funcionario.findAll({ where: {email: email}})
+   
+    if (!result[0]) {
+        console.log('Nenhum usuário encontrado')
+        return res.redirect('/login')
+    }
+
+    if (!await bcrypt.compare(senha, result[0].senha)) {
+        console.log('Senha incorreta')
+        return res.redirect('/')
+    }
+
+    console.log('Logado com sucesso')
+})
 module.exports = router
